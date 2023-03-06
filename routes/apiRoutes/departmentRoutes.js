c1aa103c184ecd36b28b7aa4d0ad92f0e787f34f
MNlegion <rkreuser30@gmail.com>
2023-03-06 11:44:46 -0500

    department query statements prepared, need to convert to ROUTER use in apiRoutes file next

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -3,15 +3,45 @@ const router = express.router();
 const db = require('../../config/connection');
 
 // GET all departments
+// Need to convert to ROUTER
+db.query(`SELECT * FROM department`, (err, rows) => {
+  console.log(rows);
+});
 
 
 // GET single department
+// Need to convert to ROUTER
+db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
 
 
 // CREATE single department
+// Need to convert to ROUTER
+const sql = (`INSERT INTO department (id, department_name)
+            VALUES (?, ?)`);
+
+const params = [1,'Executive Leadership Team'];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 
 // Delete single department
+// Need to convert to ROUTER
+db.query(`DELETE FROM department WHERE ID = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,17 +8,40 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// GET all departments
 // db.query(`SELECT * FROM department`, (err, rows) => {
 //   console.log(rows);
 // });
 
 // GET a SINGLE department
-db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(row);
-});
+// db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE a SINGLE department
+// db.query(`DELETE FROM department WHERE ID = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// CREATE a SINGLE department
+// const sql = (`INSERT INTO department (id, department_name)
+//             VALUES (?, ?)`);
+
+// const params = [1,'Executive Leadership Team'];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
