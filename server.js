30a3fe2c726268527fa3d1b6c21969f556a93f9f
MNlegion <rkreuser30@gmail.com>
2023-03-06 00:49:36 -0500

    dotenv import and require

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,17 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to MySQL DATABASE
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log('Successfully connected to the ')
+);
 
 
   // Default response for any other request (Not Found)
